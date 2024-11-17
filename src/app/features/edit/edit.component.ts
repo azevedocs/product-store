18b092761854f068902f986c5e990cd6d78e68a8
Gabriel <gabriel_azevedocs@outlook.com>
2024-11-17 19:28:40 -0300

    up15

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/service/products.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
+  productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
+  product: Product = inject(ActivatedRoute).snapshot.data['product'];
+
+  form = new FormGroup({
+    title: new FormControl<string>(this.product.title, { 
+      nonNullable: true, 
+      validators: Validators.required
+    }),
+  });
+
+  onSubmit () {
+    this.productsService.put(this.product.id, {
+      title: this.form.controls.title.value
+    })
+    .subscribe(() => {   
+      this.matSnackBar.open('Produto editado com sucesso!', 'Ok');
+
+      this.router.navigateByUrl('/');
+    });
+  }
 }
--- src/app/shared/service/products.service.ts
+++ src/app/shared/service/products.service.ts
@@ -15,8 +15,16 @@ export class ProductsService {
     return this.httpClient.get<Product[]>('http://localhost:3000/products');
   }
 
+  get(id: string) {
+    return this.httpClient.get<Product>(`http://localhost:3000/products/${id}`);
+  }
+
   post(payload: ProductPayload) {
     return this.httpClient.post('http://localhost:3000/products', payload);
+  }
+
+  put(id: string, payload: ProductPayload) {
+    return this.httpClient.put(`http://localhost:3000/products/${id}`, payload);
   }
 
 }
