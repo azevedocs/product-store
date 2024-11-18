1bcfab196821c6fee360d09ca1f2ad5b4439e582
Gabriel <gabriel_azevedocs@outlook.com>
2024-11-18 17:10:49 -0300

    up24

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -7,15 +7,16 @@ import { ProductsService } from '../../shared/services/products.service';
 import { subscribeOn } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { validateHorizontalPosition } from '@angular/cdk/overlay';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ FormComponent ],
+  imports: [ FormComponent, BackToListComponent ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
